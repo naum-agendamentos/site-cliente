e24e1701aa67dbfc5e95dcddcdafcd14575ec372
Bryan Silva <bryan.liaris@sptech.school>
2024-05-29 19:34:42 -0300

    Guilherme que fez esse commit

--- site-cliente/src/components/barChart/BarChartComponent.jsx
+++ site-cliente/src/components/barChart/BarChartComponent.jsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import styles from '../../pages/dashboard/dashboard.module.css';
-const BarChartComponent = ({ data, title }) => {
+
+const BarChartComponent = ({ data, title, color, customAxisLabels }) => {
   return (
     <div className={styles.chart}>
       <h2>{title}</h2>
       <ResponsiveContainer width="100%" height={300}>
-        <BarChart
-          data={data}
-          margin={{
-            top: 20, right: 30, left: 20, bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="pv" stackId="a" fill="#8884d8" />
-        </BarChart>
+        {data.length > 0 ? (
+          <BarChart
+            data={data}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+              dataKey={customAxisLabels ? 'customLabel' : 'name'}
+              tickLine={false}
+              axisLine={false}
+              tick={customAxisLabels ? customAxisLabels.map((label) => ({ value: label })) : null}
+              tickSize={10}
+              padding={10}
+            />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" stackId="a" fill={color || '#8884d8'} />
+          </BarChart>
+        ) : (
+          <p>No data available for this chart.</p>
+        )}
       </ResponsiveContainer>
     </div>
   );
--- site-cliente/src/pages/dashboard/dashboard.jsx
+++ site-cliente/src/pages/dashboard/dashboard.jsx
@@ -2,31 +2,103 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import styles from './dashboard.module.css';
 import BarChartComponent from '../../components/barChart/BarChartComponent';
-import NavbarBarbeiros from '../../components/navbarBarbeiro/NavbarBarbeiro';
+//import NavbarBarbeiros from '../../components/navbarBarbeiro/NavbarBarbeiro';
 
 const Dashboard = () => {
   const [data1, setData1] = useState([]);
   const [data2, setData2] = useState([]);
   const [data3, setData3] = useState([]);
   const [data4, setData4] = useState([]);
-  
+
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
     try {
-      const response1 = await axios.get('http://example.com/endpoint1');
-      setData1(response1.data);
 
-      const response2 = await axios.get('http://example.com/endpoint2');
-      setData2(response2.data);
+      const options = {
+        method: 'GET',
+        url: 'http://localhost:8080/dashboards/cortes-por-barbeiro',
+        headers: {
+          'User-Agent': 'insomnia/8.6.1',
+          Authorization: `Bearer ${sessionStorage.getItem("token")}`
+        }
+      };
+      axios.request(options)
+        .then(function (response) {
+          if (response.data.length > 0) {
+            setData1(response.data);
+          } else {
+            console.error("A resposta da API está vazia");
+          }
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
 
-      const response3 = await axios.get('http://example.com/endpoint3');
-      setData3(response3.data);
 
-      const response4 = await axios.get('http://example.com/endpoint4');
-      setData4(response4.data);
+        const options2 = {
+          method: 'GET',
+          url: 'http://localhost:8080/dashboards/lucro-por-barbeiro',
+          headers: {
+            'User-Agent': 'insomnia/8.6.1',
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`
+          }
+        };
+        axios.request(options2)
+          .then(function (response) {
+            if (response.data.length > 0) {
+              setData2(response.data);
+            } else {
+              console.error("A resposta da API está vazia");
+            }
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+
+
+          const options3 = {
+            method: 'GET',
+            url: 'http://localhost:8080/dashboards/top-servicos',
+            headers: {
+              'User-Agent': 'insomnia/8.6.1',
+              Authorization: `Bearer ${sessionStorage.getItem("token")}`
+            }
+          };
+          axios.request(options3)
+            .then(function (response) {
+              if (response.data.length > 0) {
+                setData3(response.data);
+              } else {
+                console.error("A resposta da API está vazia");
+              }
+            })
+            .catch(function (error) {
+              console.error(error);
+            });
+
+            const options4 = {
+              method: 'GET',
+              url: 'http://localhost:8080/dashboards/agendamentos-semanais',
+              headers: {
+                'User-Agent': 'insomnia/8.6.1',
+                Authorization: `Bearer ${sessionStorage.getItem("token")}`
+              }
+            };
+            axios.request(options4)
+              .then(function (response) {
+                if (response.data.length > 0) {
+                  setData4(response.data);
+                } else {
+                  console.error("A resposta da API está vazia");
+                }
+              })
+              .catch(function (error) {
+                console.error(error);
+              });
+
     } catch (error) {
       console.error('Error fetching data: ', error);
     }
@@ -35,10 +107,10 @@ const Dashboard = () => {
     <div className={styles.dashboard}>
       <h1>Dashboard</h1>
       <div className={styles.charts}>
-      <BarChartComponent data={{ labels: data1.labels, datasets: [{ label: 'Data 1', data: data1.values }] }} />
-      <BarChartComponent data={{ labels: data2.labels, datasets: [{ label: 'Data 2', data: data2.values }] }} />
-      <BarChartComponent data={{ labels: data3.labels, datasets: [{ label: 'Data 3', data: data3.values }] }} />
-      <BarChartComponent data={{ labels: data4.labels, datasets: [{ label: 'Data 4', data: data4.values }] }} />
+        <BarChartComponent data={{ labels: data1.labels, datasets: [{ label: 'Data 1', data: data1.values }] }} />
+        <BarChartComponent data={{ labels: data2.labels, datasets: [{ label: 'Data 2', data: data2.values }] }} />
+        <BarChartComponent data={{ labels: data3.labels, datasets: [{ label: 'Data 3', data: data3.values }] }} />
+        <BarChartComponent data={{ labels: data4.labels, datasets: [{ label: 'Data 4', data: data4.values }] }} />
 
       </div>
     </div>
