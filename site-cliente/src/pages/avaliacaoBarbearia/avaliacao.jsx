45124cbaa02f0804f4365fe396b262d909df796d
Bryan Silva <bryan.liaris@sptech.school>
2024-06-08 20:37:14 -0300

    parte avaliação funcionado

--- site-cliente/src/pages/avaliacaoBarbearia/avaliacao.jsx
+++ site-cliente/src/pages/avaliacaoBarbearia/avaliacao.jsx
@@ -1,49 +1,74 @@
 import React, { useState } from 'react';
 import styles from './AvaliacaoBarbearia.module.css';
-import NavbarBarbeiros from '../../components/navbarBarbeiro/NavbarBarbeiro';
+import Navbar from '../../components/navbar-pos-login/NavBar';
 import CardBarbearia from '../../components/cardBarbearia/CardBarbearia';
+import axios from "axios";
+import { toast } from "react-toastify";
 
 function AvaliacaoBarbearia() {
     const [rating, setRating] = useState(0);
     const [hover, setHover] = useState(0);
     const handleSave = () => {
-        alert(`Avaliação salva: ${rating} estrelas`);
+        const options = {
+            method: 'POST',
+            url: `http://localhost:8080/avaliacoes/1`,
+            params: { idCliente: sessionStorage.getItem("idCliente") },
+            data: {
+                qtdEstrela: rating
+            },
+            headers: {
+                'User-Agent': 'insomnia/9.2.0',
+                Authorization: `Bearer ${sessionStorage.getItem("token")}`
+            }
+        };
+        axios.request(options).then(function (response) {
+            toast.success("Barbearia avaliada! Obrigado");
+            console.log(response.data);
+        }).catch(function (error) {
+            console.error(error);
+            toast.error(error);
+        });
     };
     return (
         <div>
 
             <div class="borda-gradiente-left">
                 <div class="borda-gradiente-right">
-                    <NavbarBarbeiros />
-                    <div className={styles.container}>
+                    <div className={styles["container-avaliacao"]}>
 
-                        <div className={styles.avaliacao}>
-                            <h2 className={styles["h2-avaliacao"]}>Avaliação da Barbearia</h2>
-                            <CardBarbearia />
-                            <div className={styles.stars}>
-                                {[...Array(5)].map((star, index) => {
-                                    index += 1;
-                                    return (
-                                        <button
-                                            type="button"
-                                            key={index}
-                                            className={index <= (hover || rating) ? styles.on : styles.off}
-                                            onClick={() => setRating(index)}
-                                            onMouseEnter={() => setHover(index)}
-                                            onMouseLeave={() => setHover(rating)}
-                                        >
-                                            <span className="star">&#9733;</span>
-                                        </button>
-                                    );
-                                })}
-                            </div>
-                            <p className={styles["p-avaliacao"]}>Sua avaliação: {rating} estrelas</p>
-                            <div className={styles["container-btn"]}>
-                                <button className={styles["button-cadastrar"]} type="button"
-                                    onClick={handleSave}>CADASTRAR AVALIAÇÃO</button>
+                        <Navbar />
+                        <div className={styles["container-avalia"]}>
+
+                            <div className={styles["avaliacao"]}>
+                                <h2 className={styles["h2-avaliacao"]}>Avaliação da Barbearia</h2>
+                                <CardBarbearia />
+                                <div className={styles["stars"]}>
+                                    {[...Array(5)].map((star, index) => {
+                                        index += 1;
+                                        return (
+                                            <button
+                                                type="button"
+                                                key={index}
+                                                className={index <= (hover || rating) ? styles["on"] : styles["off"]}
+                                                onClick={() => setRating(index)}
+                                                onMouseEnter={() => setHover(index)}
+                                                onMouseLeave={() => setHover(rating)}
+                                            >
+                                                <span className={styles["star"]}>&#9733;</span>
+                                            </button>
+                                        );
+                                    })}
+                                </div>
+                                <p className={styles["p-avaliacao"]}>Sua avaliação: {rating} estrelas</p>
+                                <div className={styles["container-btn"]}>
+                                    <button className={styles["button-cadastrar"]} type="button"
+                                        onClick={handleSave}>CADASTRAR AVALIAÇÃO</button>
+                                </div>
                             </div>
                         </div>
+
                     </div>
+
                 </div>
             </div>
         </div>
