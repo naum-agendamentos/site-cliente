c34424095df1b5770680a02d6256af4aeb3ee033
Bryan Silva <125322115+BryanSilva117@users.noreply.github.com>
2024-11-24 20:48:29 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Cria uma instância do Axios com configurações personalizadas.
 const api = axios.create({
-    baseURL: process.env.API_URL || "http://localhost:8080"  // Fallback para localhost se a variável não for definida
+    baseURL: process.env.API_URL || "http://localhost/"  // Fallback para localhost se a variável não for definida
 });
 
 // Exporta a instância criada para que possa ser utilizada em outras partes do projeto.
