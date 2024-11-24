77ae8b594dd0b5f703863dfb7c220a83c393b9a6
Bryan Silva <125322115+BryanSilva117@users.noreply.github.com>
2024-11-24 20:36:54 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,37 +1,10 @@
 // Importa a biblioteca Axios.
 import axios from "axios";
 
-// Variável para armazenar a instância do Axios
-let api = null;
+// Cria uma instância do Axios com configurações personalizadas.
+const api = axios.create({
+    baseURL: process.env.API_URL || "http://localhost:8080"  // Fallback para localhost se a variável não for definida
+});
 
-// Função para pegar o IP público da instância EC2 via Metadata API
-async function getPublicIP() {
-    try {
-        const response = await axios.get('http://169.254.169.254/latest/meta-data/public-ipv4');
-        return response.data;
-    } catch (error) {
-        console.error("Erro ao obter o IP público:", error);
-        return null; // Caso haja um erro, pode-se configurar um IP padrão ou tratar da maneira desejada.
-    }
-}
-
-// Função para criar e exportar a instância do Axios com o IP público
-export async function getApi() {
-    if (!api) { // Verifica se a instância já foi criada
-        const publicIP = await getPublicIP();
-
-        if (publicIP) {
-            api = axios.create({
-                baseURL: `http://${publicIP}/`  // Configura o baseURL dinamicamente com o IP público
-            });
-        } else {
-            console.error("Não foi possível obter o IP público.");
-            return null;
-        }
-    }
-
-    return api; // Retorna a instância criada
-}
-
-// Se desejar usar diretamente a instância (não recomendado sem esperar a criação):
-export default getApi();
+// Exporta a instância criada para que possa ser utilizada em outras partes do projeto.
+export default api;
